e05376c4f4bbcdceb46e2b406169f50fc75124b3
Andrew Roth <a.roth.80@hotmail.com>
2025-04-14 16:27:54 -0700

    using blocks

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,6 +1,9 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now  # Record the start time
+  yield                   # Execute the block
+  Time.now - start_time   # Calculate and return the elapsed time
 end
+
  
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
